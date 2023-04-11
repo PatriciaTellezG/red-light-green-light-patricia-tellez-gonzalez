4109e1483e17f9efb176b72ea63dc9da5715b023
TELLEZ GONZALEZ Patricia <patricia.tellez@soprasteria.com>
2023-04-11 08:49:58 +0200

    adding form and button

--- red-light-green-light/src/pages/home.js
+++ red-light-green-light/src/pages/home.js
@@ -8,8 +8,8 @@ export class Home extends LitElement {
 
   constructor() {
     super();
-    this.homeTitle = 'Home';
-    this.userName = 'Chloe';
+    this.homeTitle = 'Create new player';
+    this.userName = '';
     
   }
 
@@ -21,14 +21,98 @@ export class Home extends LitElement {
      border-radius:1rem;
      
     }
+
+    .input {
+            top: 0px;
+            left: 0px;
+            height: 100%;
+            width: 100%;
+            border: 1px solid brown;
+            border-radius: 7px;
+            font-size: 16px;
+            padding: 0 20px;
+            outline: none;
+            background: none;
+           
+        }
+        .inputContainer {
+            position: relative; 
+            height: 45px;
+            width: 90%;
+            margin-bottom: 15px;
+            display:flex;
+            margin-left: 15px;
+        }
+        ::placeholder {
+            color: transparent;
+        }
+
+        .label {
+            position: absolute;
+            top: 15px;
+            left: 30px;
+            padding: 0 4px;
+            background-color: white;
+            color: brown;
+            font-size: 16px;
+            transition: 0.5s;
+            z-index: 0;
+            margin-left:10px;
+        }
+
+        .input:focus + .label {
+            top: -7px;
+            left: 30px;
+            z-index: 10;
+            font-size: 14px;
+            font-weight: 600;
+            color: orange;
+        }
+        .input:focus {
+            border: 2px solid orange;
+        }
+
+        .input:not(:placeholder-shown)+ .label {
+            top: -7px;
+            left: 3px;
+            z-index: 10;
+            font-size: 14px;
+            font-weight: 600;
+        }
+        button {
+            background-color: rgb(255 171 102 / 50%);
+            border-radius: 1rem;
+            padding: 0.5rem 0.5rem;
+            margin: 2rem 1.2rem;
+            border-color: transparent;
+            outline: none;
+            cursor: pointer;
+            color: brown;
+            
+        }
+    
+
   `;
 
   render() {
     return html`
     <div class="wallpaperContainer">
       <h1>${this.homeTitle}</h1>
-      <p>¡Bienvenida ${this.userName}!</p>
-    </div>`;
+      <p> ${this.userName}</p>
+    </div>
+    <div class="form">
+            <div class="containerForm">
+                <div class="inputContainer">
+                    <input type="text" class="input" placeholder="Name">
+                    <label for="" class="label">Name</label>   
+                </div> 
+            </div>
+            <div class="joinButton">
+                <button  @click="${this.joinButton}"> JOIN</button>
+            </div>
+  </div>
+    `;
+    
   }
 }
 
