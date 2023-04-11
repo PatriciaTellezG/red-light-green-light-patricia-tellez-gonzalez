abce6359066dcb73e5278dabd12bb04b2bfcbc6c
TELLEZ GONZALEZ Patricia <patricia.tellez@soprasteria.com>
2023-04-11 13:53:18 +0200

    adding some css and html

--- red-light-green-light/src/components/home.js
+++ red-light-green-light/src/components/home.js
@@ -8,7 +8,7 @@ export class Home extends LitElement {
 
   constructor() {
     super();
-    this.homeTitle = 'Create new player';
+    this.homeTitle = 'Write your name and start to play!';
     this.userName = '';
     
   }
@@ -22,12 +22,25 @@ export class Home extends LitElement {
      
     }
 
+    h1 {
+      display: flex;
+      justify-content:center;
+      padding: 10px;
+     
+    }
+
+    .welcome-icon {
+      display:flex;
+      justify-content:center;
+      width: 10rem;
+    }
+
     .input {
             top: 0px;
             left: 0px;
             height: 100%;
             width: 100%;
-            border: 1px solid brown;
+            border: 2px solid #3195c7;
             border-radius: 7px;
             font-size: 16px;
             padding: 0 20px;
@@ -52,8 +65,8 @@ export class Home extends LitElement {
             top: 15px;
             left: 30px;
             padding: 0 4px;
-            background-color: white;
-            color: brown;
+            background-color: #E0F0F3 ;
+            color: #3195c7 ;
             font-size: 16px;
             transition: 0.5s;
             z-index: 0;
@@ -66,10 +79,10 @@ export class Home extends LitElement {
             z-index: 10;
             font-size: 14px;
             font-weight: 600;
-            color: orange;
+            color: #35DCD9 ;
         }
         .input:focus {
-            border: 2px solid orange;
+            border: 2px solid #35DCD9 ;
         }
 
         .input:not(:placeholder-shown)+ .label {
@@ -80,15 +93,21 @@ export class Home extends LitElement {
             font-weight: 600;
         }
         button {
-            background-color: rgb(255 171 102 / 50%);
+            
             border-radius: 1rem;
             padding: 0.5rem 0.5rem;
             margin: 2rem 1.2rem;
             border-color: transparent;
             outline: none;
             cursor: pointer;
-            color: brown;
+            background-color: #3195c7;
             
+        }
+        a {
+          text-decoration: none;
+          color: #E0F0F3 ;
+          font-weight:400;
+          
         }
     
 
@@ -98,17 +117,19 @@ export class Home extends LitElement {
     return html`
     <div class="home-page-text">
       <h1>${this.homeTitle}</h1>
+      <img class="welcome-icon" src="./assets/welcome.png" >
       <p> ${this.userName}</p>
     </div>
     <div class="form">
             <div class="containerForm">
                 <div class="inputContainer">
-                    <input type="text" class="input" placeholder="Name">
+                    <input type="text" class="input" placeholder="Name" required>
                     <label for="" class="label">Name</label>   
                 </div> 
             </div>
             <div class="joinButton">
-                <button >  <a href="/profile">JOIN</a></button>
+                <button > <a href="/profile">JOIN</a></button>
+            </div>
                
             
   </div>
@@ -119,3 +140,4 @@ export class Home extends LitElement {
 
 customElements.define('home-component', Home);
 
+//icons taken from <a href="https://www.flaticon.com/free-icons/bank" title="bank icons">Bank icons created by Freepik - Flaticon</a>
