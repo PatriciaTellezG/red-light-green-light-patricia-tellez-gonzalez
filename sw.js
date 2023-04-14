e920dabb6f6a3936078f336cf329ebc099ddfd7d
TELLEZ GONZALEZ Patricia <patricia.tellez@soprasteria.com>
2023-04-14 10:04:48 +0200

    service worker

---
+++ sw.js
@@ -0,0 +1,47 @@
+const staticResourcesCache = "static-cache-v1";
+const resources = [
+  "/",
+  "/index.html",
+  "../assets/green.png",
+  "../assets/red.png",
+  "../assets/welcome.png",
+  
+];
+
+
+self.addEventListener("install", (e) => {
+  console.log("Intentando instalar el SW");
+  e.waitUntil(
+    caches.open(staticResourcesCache).then((cache) => {
+      return cache.addAll(resources);
+    })
+  );
+});
+
+
+self.addEventListener("activate", (e) => {
+
+  caches.keys().then((cacheNames) => {
+    return Promise.all(
+      cacheNames.map((name) => {
+        if (name !== staticResourcesCache) {
+          return caches.delete(name);
+        }
+      })
+    );
+  });
+});
+
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return (
+        cachedResponse ||
+        fetch(e.request).catch((e) => {
+          console.log(e);
+        })
+      ); 
+    })
+  );
+});
